10496bf9f0b720d273b6310d88bfcf4f68c3912c
Stephy Cat <stephycat@link.cuhk.edu.hk>
2022-01-21 11:50:35 +0000

    ADD: Support of application specific configuration files

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+# Load Envirnmental Variables.
+ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
+
 # Initialize the Rails application.
 Rails.application.initialize!
